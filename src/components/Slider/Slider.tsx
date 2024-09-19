1d30986e930405723dd0edf850bc5f19837c0c61
Denys Makarov <makarovdenys@microsoft.com>
2024-09-19 13:15:56 -0700

    title is fixed

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -10,7 +10,7 @@ const Slider = () => {
   return (
     <section className='slider-container'>
         <div className='slider-content'>
-            <h1 className='slider-title'>Welcom to Operator Connect</h1>
+            <h1 className='slider-title'>Welcome to Operator Connect</h1>
             <p className='sleder-description'>Operator Connect allows organizations to connect Microsoft Teams to the Public Switched Telephone Network (PSTN) for voice, emergency, and messaging services</p>
             <ul className='slider-list'>
                 {
